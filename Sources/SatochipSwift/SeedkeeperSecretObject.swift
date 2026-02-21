589d062dd5cf0fc58eb72478091f8edf57eb413b
Ron <ron@Rons-Mac-mini.local>
2026-02-21 00:22:56 -0800

    Fix accidentally uncommented closing braces

--- Sources/SatochipSwift/SeedkeeperSecretObject.swift
+++ Sources/SatochipSwift/SeedkeeperSecretObject.swift
@@ -66,7 +66,7 @@ public struct SeedkeeperSecretObject {
 //        }
 //        let secretHash = Crypto.shared.sha512(Array(secretBytes[1..<secretBytes.count]))
 //        return secretHash
-    }
+//    }
     
     // todo: this method only makes sense for Master Password secret
 //    public func getHmacSha512(salt: [UInt8]) -> [UInt8] {
@@ -75,6 +75,6 @@ public struct SeedkeeperSecretObject {
 //        }
 //        let hmac = Crypto.shared.hmacSHA512(data: Array(secretBytes[1..<secretBytes.count]), key: salt)
 //        return hmac
-    }
+//    }
     
 }
